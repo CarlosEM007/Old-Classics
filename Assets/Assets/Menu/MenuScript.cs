8f79bbc1f066e0ad96761e27dd89610f5737416e
Carlos <Carlos@DESKTOP-VVG8RI8>
2026-02-16 18:17:29 -0300

    feat: Sistema de controle do menu

--- Assets/Assets/Menu/MenuScript.cs
+++ Assets/Assets/Menu/MenuScript.cs
@@ -1,5 +1,8 @@
+using Assets.Assets.Shared;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
@@ -9,12 +12,73 @@ public class MenuScript : MonoBehaviour
     [SerializeField] private TextMeshProUGUI Text;
 
     [Header("Jogos")]
-    [SerializeField] private void \
+    [SerializeField] private List<Game> Games;
+
+    private int Indice = 0;
+
+    private void Start()
+    {
+        SetaCima.SetActive(false);
+        TrocarNome(Games[Indice].Name);
+    }
 
     void Update()
     {
-        
+        TrocarJogo();
+        AbrirJogo();
+        ControladorDeSetas();
     }
 
-    private 
+    private void TrocarJogo()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (Indice < Games.Count - 1)
+            {
+                Indice++;
+                TrocarNome(Games[Indice].Name);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (Indice > 0)
+            {
+                Indice--;
+                TrocarNome(Games[Indice].Name);
+            }
+        }
+    }
+
+    private void ControladorDeSetas()
+    {
+        if (Indice == Games.Count - 1)
+        {
+            SetaBaixo.SetActive(true);
+            SetaCima.SetActive(false);
+        }
+        else if (Indice == 0)
+        {
+            SetaBaixo.SetActive(false);
+            SetaCima.SetActive(true);
+        }
+        else
+        {
+            SetaBaixo.SetActive(true);
+            SetaCima.SetActive(true);
+        }
+    }
+
+    private void TrocarNome(string NomeJogo)
+    {
+        Text.text = NomeJogo;
+    }
+
+    private void AbrirJogo()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene(Games[Indice].Name);
+        }
+    }
 }
---
+++ Assets/Assets/Shared/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Assets.Shared
+{
+    [CreateAssetMenu(fileName = "Jogos", menuName = "Jogos/Jogo")]
+    public class Game : ScriptableObject
+    {
+        [SerializeField] public string Name;
+    }
+}
