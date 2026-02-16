6a68c03e36e0ae1b013f4c6fa32a05008670247e
Carlos <Carlos@DESKTOP-VVG8RI8>
2026-02-16 15:07:57 -0300

    Feat: Criação Menu.

---
+++ Assets/Assets/Menu/MenuScript.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] private GameObject SetaCima;
+    [SerializeField] private GameObject SetaBaixo;
+    [SerializeField] private TextMeshProUGUI Text;
+
+    [Header("Jogos")]
+    [SerializeField] private void \
+
+    void Update()
+    {
+        
+    }
+
+    private 
+}
