cce50429ed6578df6a6a791f37b85bdb242cf37c
Carlos <Carlos@DESKTOP-VVG8RI8>
2026-02-20 22:38:19 -0300

    feat: Criando o Arkanoid.

---
+++ Assets/Assets/Games/Arkanoid/Scripts/PlayerScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody2D rigid;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
